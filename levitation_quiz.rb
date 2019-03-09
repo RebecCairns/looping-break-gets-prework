8422099c12684e18f1f2f079c663db9eb5c91790
Rebecca Cairns <rebecca_cairns@yahoo.com>
2019-03-09 21:25:51 +0000

    Automatically backed up by Learn

--- levitation_quiz.rb
+++ levitation_quiz.rb
@@ -3,7 +3,7 @@ def levitation_quiz
 	puts "What is the spell that enacts levitation?\n"
 	answer = gets.chomp 
 	if answer == "Wingardium Leviosa"
-	  puts 
+	  puts "You passed the quiz!\n"
 end
 
 
